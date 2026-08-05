400bcc76737d242fc4c0e5be035c4f09f7bb4586
OAndrei314 <cinguscingy@gmail.com>
2026-08-05 20:53:57 +0200

    Add telemetry benchmark tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,5 +1,11 @@
 from telemetry_bench import __version__
+from telemetry_bench.benchmark import BenchmarkConfig
 
 
 def test_version_is_defined():
     assert __version__ == "0.1.0"
+
+
+def test_benchmark_config_defaults_are_small():
+    assert BenchmarkConfig.train_samples <= 1024
+    assert BenchmarkConfig.repeats <= 100
